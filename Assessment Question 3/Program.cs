544ee66515befa3162962946891df9ac9a6eaf45
Thulani <thulanig@bettech.com>
2020-07-08 20:55:40 +0200

    Initial commit

---
+++ Assessment Question 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using RestSharp;
+using RestSharp.Serialization.Json;
+
+namespace Assessment_Question_3
+{
+    class Program
+    {
+        
+        /// <summary>
+        /// Representing service endpoint
+        /// </summary>
+        private const string Service = "/api/users";
+        
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://reqres.in");
+            var request = new RestRequest(Service, Method.GET);
+            
+            var response = client.Execute(request);
+            var obj = new JsonDeserializer().Deserialize<dynamic>(response);
+            var users = obj["data"];
+
+            foreach (var user in users)
+            {
+                var id = user["id"];
+                var firstName = user["first_name"];
+                var lastName = user["last_name"];
+                
+                var path = @"\home\" + id + firstName + lastName;
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
---
+++ Assessment Question 3/README.md
@@ -0,0 +1,13 @@
+# VOSS ASSESSMENT Q3
+> The script is written using [.NET Core 3.1](https://devblogs.microsoft.com/dotnet/announcing-net-core-3-1/) 
+
+## Development Environment
+### Approved IDE
+The project is written using [Rider](https://www.jetbrains.com/rider/).
+
+## Project Setup
+### Restore from Git Repo
+
+# Building
+## ASP.NET Core
+Rider handles the build process for the application, no further action is necessary.
